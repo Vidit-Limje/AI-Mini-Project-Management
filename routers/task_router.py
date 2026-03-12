87f0e6264994c16e193038a5fd84cf7cde47a444
Vidit Limje <viditlimje@gmail.com>
2026-03-12 11:53:30 +0530

    12-03-2026 | 11:53

--- routers/task_router.py
+++ routers/task_router.py
@@ -1,6 +1,7 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException,Query
 from sqlalchemy.orm import Session
-
+from typing import Optional
+from uuid import UUID 
 from database.database import get_db
 from models.task import Task
 from schemas.task_schema import TaskCreate, TaskUpdate
@@ -25,9 +26,30 @@ def create_task(task: TaskCreate, db: Session = Depends(get_db)):
     return new_task
 
 @router.get("/")
-def get_tasks(db: Session = Depends(get_db)):
+def get_tasks(
+    status: Optional[str] = Query(None),
+    priority: Optional[str] = Query(None),
+    assignee_id: Optional[UUID] = Query(None),
+    search: Optional[str] = Query(None),
+    db: Session = Depends(get_db)
+):
 
-    tasks = db.query(Task).all()
+    query = db.query(Task)
+
+    if status:
+        query = query.filter(Task.status == status)
+
+    if priority:
+        query = query.filter(Task.priority == priority)
+
+    if assignee_id:
+        query = query.filter(Task.assignee_id == assignee_id)
+
+    if search:
+        query = query.filter(Task.task_title.ilike(f"%{search}%"))
+
+    tasks = query.all()
+
     return tasks
 
 @router.get("/{task_id}")
